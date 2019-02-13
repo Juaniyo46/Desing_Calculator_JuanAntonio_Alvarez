0c4a52a03b5a85d8de0371e2a8a2570acd172448
Juaniyo46 <juan.alvarez@escuelaestech.es>
2019-02-13 11:21:01 +0100

    Metiendo funciones

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-var num1 = 0;
-var num2 = 0;
+
+
+window.onload = function(){
+	textoPantalla = document.getElementById("pantalla");
+}
+
+var num1 = "0";
+var mostrar = 1;
+var coma = 0;
 var opera;
 
+
+
+function darNumero(numero) {
+	if (num1 == "0" && mostrar == 1) {
+		num1 == numero;
+	} else {
+		pantalla.innerHTML+=numero;
+		num1 += numero;
+	}
+
+	pulsado();
+}
